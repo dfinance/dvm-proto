2ce3a5280ad2d6ef40d923b9ea7924e1806ad495
Alex Koz <a@koz.email>
2020-03-27 19:23:29 +0300

    try fix paths for win again

--- rust/build.rs
+++ rust/build.rs
@@ -5,29 +5,26 @@ use std::path::Path;
 use std::fs::canonicalize;
 
 const OUT_DIR: &str = "src/grpc";
-const PB_DIR: &str = "../protos/";
 const PB_PATH: [&str; 2] = [
-    "vm.proto",
-    "data-source.proto",
+    "../protos/vm.proto",
+    "../protos/data-source.proto",
 ];
 
 fn main() -> Result<(), Box<dyn std::error::Error>> {
     println!("cargo:rerun-if-changed=src/build.rs");
 
-	 let protos = canonicalize(PB_DIR).map_err(|err| eprintln!("ERROR: protos-sources: {:?}", err)).unwrap();
-	 eprintln!("canonicalized protos-sources path: {:?}", protos);
-	 std::fs::read_dir(&protos).map(|res|res.for_each(|item| eprintln!("{:?}", item))).unwrap();
+    let protos = canonicalize("../protos/").map_err(|err| eprintln!("invalid path: {:?}", err)).unwrap();
+    eprintln!("canonicalized protos-sources path: {:?}", protos);
 
     for path in PB_PATH.iter() {
         println!("rerun-if-changed={}", path);
         println!("cargo:rerun-if-changed={}", path);
-        eprintln!("rel path: {}", path);
+
         let mut sourcefile = protos.clone();
         sourcefile.push(path);
-		  eprintln!("~ path: {:?}", sourcefile);
-		  let sourcefile = canonicalize(&sourcefile).unwrap();
-        eprintln!("canonicalized path: {:?}", &sourcefile);
-        let proto_path: &Path = sourcefile.as_ref();
+        eprintln!("sourcefile path: {:?}", sourcefile);
+
+        let proto_path: &Path = path.as_ref();
         let proto_dir = proto_path
             .parent()
             .expect("proto file should reside in a directory");
